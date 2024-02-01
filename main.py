5a0dbfae9366b8014429c08ab9d846d59b75ec69
Vicente Lermanda <vlermanda@MacBook-Air-2.local>
2024-02-01 12:34:36 -0300

    handling env vars

--- main.py
+++ main.py
@@ -15,7 +15,17 @@ parser.add_argument(
         help="Run the test prompt selection.")
 args = parser.parse_args()
 
+prompt = "PROMPT_FILE"
+
 if args.test_prompts:
+    if prompt in os.environ:
+        raise EnvironmentError(f'''The environment variable '{prompt}'
+                               is already set. Remove it to run a new
+                               prompt selection.''')
+
     prompts_dir = os.getenv('TEST_PROMPTS_DIR', './data/tests/prompts/')
     results_dir = os.getenv('TEST_RESULTS_DIR', './data/tests/results/')
     run_prompt_selection(prompts_dir, results_dir, client)
+    exit(0)
+
+prompt = os.getenv('PROMPT_FILE', './selected_prompt.txt')
